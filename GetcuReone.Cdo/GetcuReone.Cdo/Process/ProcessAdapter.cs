39cf499d8ea23562fc3d43508dd4a1bbd00dbb9f
Juel <GetcuReone@hotmail.com>
2020-11-14 03:34:50 +0300

    added IProcess

---
+++ GetcuReone.Cdo/GetcuReone.Cdo/Process/ProcessAdapter.cs
@@ -0,0 +1,33 @@
+﻿using GetcuReone.Cdi;
+
+namespace GetcuReone.Cdo.Process
+{
+    /// <summary>
+    /// Adapter for <see cref="IProcess"/>.
+    /// </summary>
+    public sealed class ProcessAdapter : GrAdapterProxyBase<IProcess>
+    {
+        /// <summary>
+        /// Adapter name.
+        /// </summary>
+        protected override string AdapterName => nameof(ProcessAdapter);
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public ProcessAdapter() : base(() => new ProcessService())
+        {
+        }
+
+        /// <summary>
+        /// Start process.
+        /// </summary>
+        /// <param name="process"></param>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public System.Diagnostics.Process Start(string process, string args)
+        {
+            return CreateProxy().Start(process, args);
+        }
+    }
+}
---
+++ GetcuReone.Cdo/GetcuReone.Cdo/Process/ProcessService.cs
@@ -0,0 +1,14 @@
+﻿namespace GetcuReone.Cdo.Process
+{
+    /// <summary>
+    /// Service for process.
+    /// </summary>
+    public class ProcessService : IProcess
+    {
+        /// <inheritdoc/>
+        public System.Diagnostics.Process Start(string processName, string args)
+        {
+            return System.Diagnostics.Process.Start(processName, args);
+        }
+    }
+}
